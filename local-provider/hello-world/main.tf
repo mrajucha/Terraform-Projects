cdb6386c80e2add1175baa569c602c61d8722410
Manoj <manoj.raju-chaudhary@capgemini.com>
2023-06-03 12:48:24 +0530

    modified local provider file hello-world

--- local-provider/hello-world/main.tf
+++ local-provider/hello-world/main.tf
@@ -1,4 +1,28 @@
-resource "local_file" "hello-World" { //
-  filename = "hello.txt"                                    //path/name of the file name to be created
-  content = "Hello! this is my first terraform project"     // Content inside the file mentioned above
+resource "local_file" "hello-world" {                       # manage local resources files 
+  filename = "hello.txt"                                    #path/name of the file name to be created
+  #content = "Hello! this is my first terraform project"     # Content inside the file mentioned above
+  content = var.content
+  directory_permission = "0700"
+  file_permission = "0700"
+  #sensitive_content = "My key is 7040"
+
+}
+
+resource "random_password" "passwords" {
+  length = 5
+  special = true
+  
+}
+
+output "hello-world-op" {                         #to print the output into console
+    value = local_file.hello-world.content
+}
+
+output "passwords-op" {
+
+  value = random_password.passwords.result
+  sensitive = true
+}
+variable "content" {
+  default = "Hello! this is my first terraform project. The password length is ${random_password.passwords.length}" 
 }
